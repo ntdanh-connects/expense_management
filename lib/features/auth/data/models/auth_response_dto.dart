5c327d7b31612792183399e8e4c8fe50c7cdb5aa
Ngo Thanh Danh <danhnt@hasaki.vn>
2026-05-30 13:53:50 +0700

    Done improve bug log

--- lib/features/auth/data/models/auth_response_dto.dart
+++ lib/features/auth/data/models/auth_response_dto.dart
@@ -6,7 +6,7 @@ part 'auth_response_dto.freezed.dart';
 
 @freezed
 abstract class AuthResponseDto with _$AuthResponseDto{
-  @JsonSerializable(fieldRename: FieldRename.snake)
+  @JsonSerializable(fieldRename: FieldRename.snake, checked: true)
   const factory AuthResponseDto({
     required String message,
     required String accessToken,   
@@ -21,7 +21,7 @@ abstract class AuthResponseDto with _$AuthResponseDto{
 
 @freezed
 abstract class UserDataDto with _$UserDataDto {
-  @JsonSerializable(fieldRename: FieldRename.snake)
+  @JsonSerializable(fieldRename: FieldRename.snake, checked: true)
   const factory UserDataDto({
     required String userId,        
     required String email,
@@ -36,20 +36,20 @@ abstract class UserDataDto with _$UserDataDto {
 
 @freezed
 abstract class UserProfileDto with _$UserProfileDto {
-  @JsonSerializable(fieldRename: FieldRename.snake)
+  @JsonSerializable(fieldRename: FieldRename.snake, checked: true)
   const factory UserProfileDto({required String fullName, String? avatarUrl}) = _UserProfileDto;
   factory UserProfileDto.fromJson(Map<String, dynamic> json) => _$UserProfileDtoFromJson(json);
 }
 
 @freezed
 abstract class UserPreferenceDto with _$UserPreferenceDto {
-  @JsonSerializable(fieldRename: FieldRename.snake)
+  @JsonSerializable(fieldRename: FieldRename.snake, checked: true)
   const factory UserPreferenceDto({
     required String language,
     required String theme,
     required String currency,
     required String timezone,
-    required int financialStartDay,
+    int? financialStartDay,
   }) = _UserPreferenceDto;
 
   factory UserPreferenceDto.fromJson(Map<String, dynamic> json) => 
